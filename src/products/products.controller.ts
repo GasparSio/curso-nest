ed283ab6712694a912b83c9e244a8c27239d845a
Gaspar Sio <sio.gaspar@gmail.com>
2024-06-17 17:53:46 +0200

    adding status code

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -7,6 +7,8 @@ import {
   Post,
   Put,
   Query,
+  HttpStatus,
+  HttpCode,
 } from '@nestjs/common';
 import { ProductsService } from './products.service';
 
@@ -19,6 +21,7 @@ export class ProductsController {
   //   return 'Hello from the products endpoint!';
   // }
   @Get()
+  @HttpCode(HttpStatus.OK)
   async getAll(
     @Query('limit') limit: number = 100, //el 100 es el default value
     @Query('offset') offset: number = 0, //el 0 es el default value
@@ -29,22 +32,26 @@ export class ProductsController {
 
   //endpoint with only ONE parameter
   @Get(':productId')
+  @HttpCode(HttpStatus.ACCEPTED)
   async getOne(@Param('productId') productId: string): Promise<string> {
     const product = await this.productsService.getOne(productId);
     return product;
   }
 
   @Post()
+  @HttpCode(HttpStatus.CREATED)
   async create(@Body() payload: any): Promise<any> {
     const newProduct = await this.productsService.create(payload);
     return newProduct;
   }
   @Put(':id')
+  @HttpCode(HttpStatus.OK)
   async update(@Param('id') id: string, @Body() payload: any): Promise<any> {
     const productUpdated = await this.productsService.update(id, payload);
     return productUpdated;
   }
   @Delete(':id')
+  @HttpCode(HttpStatus.OK)
   async delete(@Param('id') id: string): Promise<any> {
     const productDeleted = await this.productsService.delete(id);
     return productDeleted;
